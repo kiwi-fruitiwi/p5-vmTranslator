8b28313252e3eeefd7a48672cf6d51bcddb636fb
kiwi-fruitiwi <zane.tian@gmail.com>
2022-05-01 01:56:56 -0700

    add template project

---
+++ sketch.js
@@ -0,0 +1,64 @@
+/**
+ *  @author kiwi
+ *  @date 2022.05.01
+ *
+ *  an implementation of project 7 and 8 from Coursera's nand2tetris II
+ *  class in p5.js to reinforce the concepts after completing the project in
+ *  python
+ *
+ */
+let font
+let instructions
+
+
+function preload() {
+    font = loadFont('data/consola.ttf')
+}
+
+
+function setup() {
+    let cnv = createCanvas(600, 300)
+    cnv.parent('#canvas')
+    colorMode(HSB, 360, 100, 100, 100)
+    textFont(font, 14)
+
+    /* initialize instruction div */
+    instructions = select('#ins')
+    instructions.html(`<pre>
+        [] → 🐳🥝
+        z → freeze sketch</pre>`)
+
+    noLoop()
+}
+
+
+function draw() {
+    background(234, 34, 24)
+    displayDebugCorner()
+}
+
+
+/** 🧹 shows debugging info using text() 🧹 */
+function displayDebugCorner() {
+    const LEFT_MARGIN = 10
+    const DEBUG_Y_OFFSET = height - 10 /* floor of debug corner */
+    const LINE_SPACING = 2
+    const LINE_HEIGHT = textAscent() + textDescent() + LINE_SPACING
+    fill(0, 0, 100, 100) /* white */
+    strokeWeight(0)
+
+    text(`frameCount: ${frameCount}`,
+        LEFT_MARGIN, DEBUG_Y_OFFSET - LINE_HEIGHT)
+    text(`frameRate: ${frameRate().toFixed(1)}`,
+        LEFT_MARGIN, DEBUG_Y_OFFSET)
+}
+
+
+function keyPressed() {
+    /* stop sketch */
+    if (key === 'z') {
+        noLoop()
+        instructions.html(`<pre>
+            sketch stopped</pre>`)
+    }
+}
